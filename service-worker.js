fd59059275bf88cf66d09cab049ca2c7c355612c
noorelahmanifestofinal <noorelahmanifestofinal@atomicmail.io>
2025-06-22 11:05:11 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,1 @@
-
-self.addEventListener("install", function(e) {
-  e.waitUntil(
-    caches.open("nwr5-badge").then(function(cache) {
-      return cache.addAll(["/", "/index.html", "/style.css", "/dapp.js"]);
-    })
-  );
-});
-self.addEventListener("fetch", function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
-});
+// placeholder
